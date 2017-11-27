dbc1bbb50af2dd4ebfa7b7fe3873a4096dda1fc3
AndeyGordeev <prolodgy@yandex.ru>
2017-11-27 04:26:10 +0300

    Sign up view, templates, etc

--- TestProject/user/urls.py
+++ TestProject/user/urls.py
@@ -5,5 +5,6 @@ from user import views
 urlpatterns = [
     url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
+    url(r'^signeup/$', views.SignUpView.as_view(), name='signeup'),
     url('^', include('django.contrib.auth.urls')),
 ]

fbd6b02c0c65d3200996da9e3d3d4c32e0d7bb9c
AndeyGordeev <prolodgy@yandex.ru>
2017-11-27 19:50:30 +0300

    Dashboard requies login

--- TestProject/user/urls.py
+++ TestProject/user/urls.py
@@ -3,7 +3,7 @@ from  django.conf.urls import url, include
 from user import views
 
 urlpatterns = [
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
+    url(r'^dashboard/$', views.Dashboard.as_view(), name='dashboard'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^signeup/$', views.SignUpView.as_view(), name='signeup'),
     url('^', include('django.contrib.auth.urls')),

5926d4538cfa3ecb46a12c9b115c937e6b722245
AndeyGordeev <prolodgy@yandex.ru>
2017-11-27 03:10:59 +0300

    Dashboard placeholder and login redirect setting

--- TestProject/user/urls.py
+++ TestProject/user/urls.py
@@ -1,5 +1,8 @@
 from  django.conf.urls import url, include
 
+from user import views
+
 urlpatterns = [
+    url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url('^', include('django.contrib.auth.urls')),
 ]
